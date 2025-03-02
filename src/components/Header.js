634123d9a43845665f74934119987ecc6c9f031d
Farish Noorhisham <farishnoorhisham81@gmail.com>
2025-03-02 20:36:45 +0800

    fix header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,30 @@
-import React from "react";
-import { Link } from "react-router-dom"; 
-import "../styles/Header.css";
-import logoImage from "../assets/logo-image.png";
-
-
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Header.css';
+import logoImage from '../assets/logo-image.png';
 
 function Header() {
   return (
-       <header className="header">
-    <div className="logo-image">
-      <img src={logoImage} alt="Rice Reviews Logo" />
-    </div>
-    <h1>
-      <Link to="/" className="header-title">Rice Reviews</Link>
-    </h1>
-  <nav>
-      <Link to="#home">Home</Link>
-      <Link to="#about">About Us</Link>
-      <Link to="#download" className="download-btn">Download</Link>
-    </nav>
-  </header>
- 
-
+    <header className="header">
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: 15,
+        }}>
+        <div className="logo-image">
+          <img src={logoImage} alt="Rice Reviews Logo" />
+        </div>
+        <Link to="/" className="header-title">
+          Rice Reviews
+        </Link>
+      </div>
+      <nav>
+        <Link to="#home">Home</Link>
+        <Link to="#about">About Us</Link>
+      </nav>
+    </header>
   );
 }
 
