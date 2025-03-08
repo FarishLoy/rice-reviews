9720774e464fc5d67e6519b50ea2c7343ffd4661
Farish Noorhisham <farishnoorhisham81@gmail.com>
2025-03-09 00:41:15 +0800

    add explore section

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import HeroSection from "./components/HeroSection";
-import VideoReviews from "./components/VideoReviews";
-import MapSection from "./components/MapSection";
-import Footer from "./components/Footer";
-import "./styles/App.css";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import VideoReviews from './components/VideoReviews';
+import MapSection from './components/MapSection';
+import Footer from './components/Footer';
+import './styles/App.css';
 import PrivacyPolicy from './components/PrivacyPolicy';
-import TermsOfService from "./components/TermsOfService";
+import TermsOfService from './components/TermsOfService';
+import ExploreSection from './components/ExploreSection';
 
 const Homepage = () => {
   return (
     <>
-        <HeroSection/>
-        <VideoReviews/>
-        <MapSection/>
+      <HeroSection />
+      <VideoReviews />
+      <MapSection />
+      <ExploreSection />
     </>
-  )
-}
+  );
+};
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header /> 
-        <Routes >
-         <Route path="/privacy-policy" element={<PrivacyPolicy />} /> 
-         <Route path="/terms-of-service" element={<TermsOfService />} />
-         <Route path="/" element={<Homepage />} />
+        <Header />
+        <Routes>
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms-of-service" element={<TermsOfService />} />
+          <Route path="/" element={<Homepage />} />
         </Routes>
         <Footer />
       </div>
---
+++ src/components/ExploreSection.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useRef } from 'react';
+import '../styles/FeatureSection.css';
+import { useInView, motion, useMotionValue, animate } from 'framer-motion';
+import screenshot4 from '../assets/screenshot4.png';
+
+function ExploreSection() {
+  const imageContainerRef = useRef(null);
+
+  const isInView = useInView(imageContainerRef, {
+    amount: 0.5,
+  });
+  const opacity = useMotionValue(0);
+
+  useEffect(() => {
+    if (isInView) {
+      const animation = animate(opacity, [0, 1], { duration: 1.5 });
+      animation.play();
+    } else {
+      const animation = animate(opacity, [1, 0], { duration: 1.5 });
+      animation.play();
+    }
+  }, [isInView]);
+
+  return (
+    <section className="feature-container">
+      <motion.div
+        style={{ opacity }}
+        ref={imageContainerRef}
+        className="screenshot-images second-section">
+        <img src={screenshot4} alt="Video Reviews" />
+      </motion.div>
+      <motion.div
+        style={{ opacity }}
+        className="description-container left-aligned">
+        <h2>Explore New Restaurants</h2>
+        <p>Explore new restaurants through reviews</p>
+        <p>Like and bookmark reviews to put the restaurant on your map</p>
+        <p>
+          Select your favorite cuisines to see recommendations to your liking
+        </p>
+      </motion.div>
+    </section>
+  );
+}
+
+export default ExploreSection;
